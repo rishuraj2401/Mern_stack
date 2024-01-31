dcfc3929beeb4814f1144613c3f0bbf538f160bb
rishuraj2401 <rishuraj2401sinha@gmail.com>
2024-01-31 10:55:38 +0530

    nodejs1

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hello CodeSandbox");
+var app = require("express");
+const bodyParser = require("body-parser");
+// const { default: mongoose } = require("mongoose");
+var app = app();
+// var mong = require("mongoose");
+// mongoose.connect("mongodb://localhost:27017/test");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.get("/", function (req, res) {
+  res.send("Hello world!");
+});
+app.post("/p", (req, res) => {
+  const { var1, var2 } = req.body;
+  console.log("message:", req.body);
+  res.send(var1);
+});
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
